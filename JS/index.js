c8b6ba5fe546c0b66c7c1038bdb40f75f1a25aa3
Shantaroy07 <shantaroy148@gmail.com>
2022-08-22 21:05:37 +0600

    selected list modified

--- JS/index.js
+++ JS/index.js
@@ -9,11 +9,14 @@ for (let i = 0; i < elements.length; i++) {
         const text = elements[i].parentNode.childNodes[1].innerText;
         elements[i].disabled = true;
         const listField = document.getElementById("list-field");
-        const li = document.createElement("li");
-        li.innerText = text;
-        listField.appendChild(li);
+        const tr = document.createElement("tr");
+        console.log(tr);
+        tr.innerHTML = `
+        <td>${(listArray.length) + 1}</td>
+        <td>${text}</td>`
+        listField.appendChild(tr);
 
-        listArray.push(li);
+        listArray.push(tr);
 
     });
 }
