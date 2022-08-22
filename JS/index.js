90d83b7200f163b245d59c802cef0fce51c7883f
Shantaroy07 <shantaroy148@gmail.com>
2022-08-22 21:20:23 +0600

    condition added

--- JS/index.js
+++ JS/index.js
@@ -8,15 +8,22 @@ for (let i = 0; i < elements.length; i++) {
     elements[i].addEventListener('click', function () {
         const text = elements[i].parentNode.childNodes[1].innerText;
         elements[i].disabled = true;
-        const listField = document.getElementById("list-field");
+        const listBodyField = document.getElementById("list-field");
         const tr = document.createElement("tr");
         console.log(tr);
         tr.innerHTML = `
         <td>${(listArray.length) + 1}</td>
-        <td>${text}</td>`
-        listField.appendChild(tr);
-
+        <td>${text}</td>`;
         listArray.push(tr);
+        if ((listArray.length) <= 5) {
+            listBodyField.appendChild(tr);
+        }
+        else {
+            alert('Players are more than 5');
+            return;
+        }
+
+
 
     });
 }
